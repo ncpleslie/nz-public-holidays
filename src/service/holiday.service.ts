d21252c2146c5c0b7b7b8c5798c5d1134415a53d
Nick Leslie <ncpleslie@gmail.com>
2023-01-16 21:15:33 +1300

    Results should be sorted

--- src/service/holiday.service.ts
+++ src/service/holiday.service.ts
@@ -1,6 +1,7 @@
 import HolidayDto from "../models/dtos/holiday_dto.model";
 import IHolidayDomain from "../domain/interfaces/holiday_domain.interface";
 import IHolidayService from "./interfaces/holiday_service.interface";
+import Holiday from "../models/entities/holiday.model";
 
 export default class HolidayService implements IHolidayService {
   constructor(protected holidayDomain: IHolidayDomain) {}
@@ -8,7 +9,7 @@ export default class HolidayService implements IHolidayService {
   public async getAllHolidaysAsync(): Promise<Record<string, HolidayDto[]>> {
     const holidays = await this.holidayDomain.getHolidaysAsync();
 
-    return holidays.reduce(
+    return this.sortHolidays(holidays).reduce(
       (prevHolidays: Record<string, HolidayDto[]>, currentHoliday) => {
         const currentHolidayDto = currentHoliday.toDto();
         const yearKey = currentHolidayDto.date.substring(0, 4);
@@ -29,6 +30,16 @@ export default class HolidayService implements IHolidayService {
   public async getHolidaysByYearAsync(year: Date): Promise<HolidayDto[]> {
     const holidays = await this.holidayDomain.getHolidaysAsync(year);
 
-    return holidays.map((holiday) => holiday.toDto());
+    return this.sortHolidays(holidays)
+      .sort((aHoliday, bHoliday) => {
+        return aHoliday.date.getTime() - bHoliday.date.getTime();
+      })
+      .map((holiday) => holiday.toDto());
+  }
+
+  private sortHolidays(holidays: Holiday[]) {
+    return holidays.sort((aHoliday, bHoliday) => {
+      return aHoliday.date.getTime() - bHoliday.date.getTime();
+    });
   }
 }
